b2e18acc66a9b67856a2f70946ccebfc7b08f277
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-20 16:49:16 +0300

    Update run-cassandra.sh

--- run-cassandra.sh
+++ run-cassandra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker network create dulher-cassandra-network
-docker run --name cassandra-node --network dulher-cassandra-network -d cassandra:latest
+docker run --name cassandra-node --network dulher-cassandra-network -p 9042:9042 -d cassandra:latest
 sleep 70s
