8f949a2ee8e677fe77831131aef3ed3bd662e814
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-20 17:38:54 +0300

    Add files via upload

---
+++ write_amazon_dataset_to_cassandra.py
@@ -0,0 +1,68 @@
+import csv
+
+from cassandra.cluster import Cluster
+
+
+class CassandraClient:
+    def __init__(self, host, port, keyspace):
+        self.host = host
+        self.port = port
+        self.keyspace = keyspace
+        self.session = None
+
+    def connect(self):
+        cluster = Cluster([self.host], port=self.port)
+        self.session = cluster.connect(self.keyspace)
+
+    def execute(self, query):
+        self.session.execute(query)
+
+    def close(self):
+        self.session.shutdown()
+
+    def write_data(self, product_id, review_id, review_headline, review_body, star_rating, review_date, customer_id,
+                   verified_purchase):
+        query1 = "INSERT INTO product_reviews_text (product_id, review_id, review_headline, review_body, star_rating)" \
+                 " VALUES ('%s', '%s', '%s', '%s', %d)" % (product_id, review_id, review_headline, review_body,
+                                                           star_rating)
+        query2 = "INSERT INTO product_reviews_date (product_id, review_id, review_date)" \
+                 " VALUES ('%s', '%s', %s)" % (product_id, review_id, review_date)
+        query3 = "INSERT INTO customer_reviews_text (customer_id, review_id, review_headline, review_body)" \
+                 " VALUES ('%s', '%s', '%s', '%s')" % (customer_id, review_id, review_headline, review_body)
+        query4 = "INSERT INTO customer_reviews_rating (customer_id, review_id, review_date, star_rating, " \
+                 "verified_purchase) VALUES ('%s', '%s', %s, %d, %r)" % (customer_id, review_id, review_date,
+                                                                         star_rating, verified_purchase)
+        self.execute(query1)
+        self.execute(query2)
+        self.execute(query3)
+        self.execute(query4)
+
+
+def write_dataset(client):
+    dataset_file = "sample_us.tsv"
+    with open(dataset_file) as file:
+        tsv_file = csv.reader(file, delimiter="\t")
+        for line in tsv_file:
+            marketplace, customer_id, review_id, product_id, product_parent, product_title, product_category, \
+            star_rating, helpful_votes, total_votes, vine, verified_purchase, review_headline, review_body, \
+            review_date = line
+            if marketplace == 'marketplace':
+                continue
+            verified_purchase = verified_purchase == 'Y'
+            star_rating = int(star_rating)
+            client.write_data(product_id, review_id, review_headline, review_body, star_rating, review_date,
+                              customer_id, verified_purchase)
+
+
+def main():
+    host = 'localhost'
+    port = 9042
+    keyspace = 'amazon_dulher'
+    client = CassandraClient(host, port, keyspace)
+    client.connect()
+    write_dataset(client)
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ write_datset.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 write_amazon_dataset_to_cassandra.py
