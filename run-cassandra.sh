7e841ff406b5262657a7125c456db9aa8becb86f
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-22 19:21:24 +0300

    Update run-cassandra.sh

--- run-cassandra.sh
+++ run-cassandra.sh
@@ -3,6 +3,10 @@
 docker network create dulher-cassandra-network
 docker run --name cassandra-node --network dulher-cassandra-network -p 9042:9042 -d cassandra:latest
 sleep 70s
+echo "Creating keyspace and tables"
 docker run -it --network dulher-cassandra-network --rm ddl_image
-. ./write_dataset.sh
+cat "Writing data into tables"
+./write_dataset.sh
+echo "Starting API"
 docker run --name cassandra-flask --network dulher-cassandra-network -p 8080:8080 -d cassandra_api
+echo "Send GET requests on port 8080"
